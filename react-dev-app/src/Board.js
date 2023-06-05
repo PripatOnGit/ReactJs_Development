0c01085a42a26e003059bf47a77be6939950b6ee
PripatOnGit <patil.priyu26@gmail.com>
2023-06-05 11:13:47 +0530

    creating board for the game

--- react-dev-app/src/Board.js
+++ react-dev-app/src/Board.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function Board() {
   return (
-    <div className="App">
+    <div>
       <h1>Tic-Tac-Toe Game!</h1>
+      <button className="square">1</button>
     </div>
   );
 }
-
-export default App;
